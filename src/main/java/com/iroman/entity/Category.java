d0fdcfba1e399c38352945d37105f0a9fb266a81
David Vera <dveracabanillas@gmail.com>
2024-08-28 22:35:18 -0500

    add category entity

---
+++ src/main/java/com/iroman/entity/Category.java
@@ -0,0 +1,72 @@
+package com.iroman.entity;
+
+import java.time.LocalDateTime;
+
+public class Category {
+    private Long id;
+    private String name;
+    private String description;
+    private String urlKey;
+    private String state;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Category() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrlKey() {
+        return urlKey;
+    }
+
+    public void setUrlKey(String urlKey) {
+        this.urlKey = urlKey;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
